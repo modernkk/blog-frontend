feffcdfb9e73e599db783f1681a1d6fce551680f
xuke <blk1314@126.com>
2020-12-21 16:51:05 +0800

    use env

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -130,7 +130,7 @@ export default {
       try {
         await axios({
           method: 'POST',
-          url: 'http://localhost:1337/contacts',
+          url: `${this.GRIDSOME_API_URL}/contacts`,
           data: this.form
         })
         window.alert('发送成功')
