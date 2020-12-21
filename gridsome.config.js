36b51f90d850e2ea34b4ffacd81aa167f2b7c268
xuke <blk1314@126.com>
2020-12-21 16:57:41 +0800

    http://117.50.2.54:1337

--- gridsome.config.js
+++ gridsome.config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       use: '@gridsome/source-strapi',
       options: {
-        apiURL: process.env.GRIDSOME_API_URL,
+        apiURL: 'http://117.50.2.54:1337',
         queryLimit: 1000, // Defaults to 100
         contentTypes: ['post', 'tag'],
         singleTypes: ['general'],
